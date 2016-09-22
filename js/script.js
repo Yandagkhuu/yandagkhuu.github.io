62560ea2a92d5487fc3cde1c995ea4e2159cd384
Yandagkhuu <yandagkhuu@outlook.com>
2016-09-23 00:29:12 +0900

    Changed interest section to portfolio

--- js/script.js
+++ js/script.js
@@ -19,10 +19,3 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
   });
 });
 
-jQuery(document).ready(function(){
-	jQuery('.skillbar').each(function(){
-		jQuery(this).find('.skillbar-bar').animate({
-			width:jQuery(this).attr('data-percent')
-		},6000);
-	});
-});
